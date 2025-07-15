9dbf0e51a13ded824f04d0058c0cecc638eb5946
conscodess <constanzacarabias@gmail.com>
2025-07-14 20:03:32 -0600

    changes to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/svelte-about-me',    // right here! your repository name
   plugins: [svelte()],
 })
